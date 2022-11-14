7fda98831088cc49fa52b907dd319a0abc5e87de
wangweiyou <wangcuisun@outlook.com>
2022-11-14 17:42:20 +0800

    改造成dubbo项目

--- dubbo-service/src/main/java/com/wang/service/impl/UserServiceImpl.java
+++ dubbo-service/src/main/java/com/wang/service/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.wang.service.impl;
 
 import com.wang.service.UserService;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
 
-@Service
+@Service //将这个类提供的方法对外发布，发布到zookeeper中
 public class UserServiceImpl implements UserService {
     @Override
     public String sayHello() {
